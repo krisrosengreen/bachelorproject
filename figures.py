1814d07d19e085ceea576a309c46de3a689bd47c
krisrosengreen <rosengreenpedersen@gmail.com>
2023-04-12 13:43:22 +0200

    Renaming

---
+++ figures.py
@@ -0,0 +1,2 @@
+from main import init_scf_calculation
+from qe import calculate_energies
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ TEMPLATE = "si.bands.template"
 FILENAME = "si.bandspy.in"
 FILEOUTPUT = "si.bandspy.out"
 
-AUTOGRID_FILENAME = "si.bands.autogrid"
+AUTOGRID_FILENAME = "si.scf.autogrid"
 
 PP_FILENAME = "si_bands_pp.in"
 PP_FILEOUTPUT = "si_bands_pp.out"
